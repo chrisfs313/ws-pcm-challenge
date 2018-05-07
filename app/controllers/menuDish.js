84bb68dd539e1cccbd843c8a0f87d4fe9eb1ef05
Cristian Flores Shute <chris_fs313@hotmail.com>
2018-05-07 20:10:58 +0000

    added upload images

--- app/controllers/menuDish.js
+++ app/controllers/menuDish.js
@@ -2,6 +2,15 @@ var mongoose = require('mongoose');
 
 var MenuDishModel = require('../models/menuDish');
 
+var AWS = require('aws-sdk');
+var uuid = require('node-uuid');
+
+AWS.config.update({
+    accessKeyId: "AKIAJ2WNTJUBNBVGXBFA",
+    secretAccessKey: "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",
+    region: "sa-east-1"
+});
+
 var MenuDish = (function () {
     
     function list(req, res, next) {
@@ -39,13 +48,52 @@ var MenuDish = (function () {
                 });
         }
         else {
-           MenuDishModel.save(req.body, function (e, d) {
-                if (e) {
-                    next(e);
-                } else {
-                    res.json(d);
-                }
-            });
+            var files = req.body.imageData;
+        
+            if (files) {
+                var s3 = new AWS.S3();
+                var fileBody = new Buffer(files, 'base64');
+                var keyName = 'file-' + uuid.v4() + ".jpg";
+                var params = {
+                    Bucket: 'pcm-challenge-s3', /* required */
+                    Key: keyName, /* required */
+                    ACL: 'public-read',
+                    Body: fileBody,
+                    ContentEncoding: 'base64',
+                    ContentType: req.body.mimeType
+                };
+                s3.putObject(params, function (err, data) {
+                    if (err) {
+                        // console.log(err, err.stack);
+                        next(err);
+                    } else {
+                        var urlDir = "https://s3-sa-east-1.amazonaws.com/pcm-challenge-s3/" + keyName;
+                        
+                        // Now change the galeria object
+                        req.body.imageUrl = urlDir;
+                        
+                        MenuDishModel.save(req.body, function (e, d) {
+                            if (e) {
+                                next(e);
+                            } else {
+                                d.imageUrl = urlDir;
+                                res.json(d);
+                            }
+                        });
+                    }
+                });
+            }
+            else {
+                MenuDishModel.save(req.body, function (e, d) {
+                    if (e) {
+                        next(e);
+                    } else {
+                        res.json(d);
+                    }
+                });
+            }
+            
+           
         }
     }
     
